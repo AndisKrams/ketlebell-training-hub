a71feebfff35eb2ace7068ed4b2343781dc3d522
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-01 14:15:29 +0100

    Update readme

--- kettlebell_hub/settings.py
+++ kettlebell_hub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(woxw!*)ml8)6-5!hk!&i)#l97&ey6mg^m+s2rkgox#op#9&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
