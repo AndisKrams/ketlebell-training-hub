9644a8a8186b3094e85d4eba3b434b5f670b9595
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-07 14:22:05 +0100

    Create head for base.html

--- kettlebell_hub/settings.py
+++ kettlebell_hub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(woxw!*)ml8)6-5!hk!&i)#l97&ey6mg^m+s2rkgox#op#9&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'kettlebell_shop',
             ],
         },
     },
