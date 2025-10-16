59cb33ffbf92d1df226f8da4d44ff2b720eb98ce
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-16 18:01:50 +0100

    Style shop page layout

--- kettlebell_shop/models.py
+++ kettlebell_shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Kettlebell(models.Model):
     WEIGHT_CHOICES = [
-        (8, '8 kg'), (12, '12 kg'), (16, '16 kg'), (20, '20 kg'),
+        (8, '8 kg '), (12, '12 kg'), (16, '16 kg'), (20, '20 kg'),
         (24, '24 kg'), (28, '28 kg'), (32, '32 kg'), (36, '36 kg'),
         (40, '40 kg'), (48, '48 kg')
         ]
