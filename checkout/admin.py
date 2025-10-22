114e12b3f56ee82774f3db87f67fb4f0dc56bfdb
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-22 16:32:23 +0100

    Added checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'full_name', 'date', 'total')
+    search_fields = ('order_number', 'full_name', 'email')
+
+
+@admin.register(OrderLineItem)
+class OrderLineItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product_name', 'quantity', 'price')
+    search_fields = ('product_name',)
