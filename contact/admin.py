087cac3a08b7f0108390e93fe95edd0d1dbb722b
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-26 16:11:52 +0000

    Updated admin to display message text

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,20 @@ from .models import ContactMessage, ContactSettings
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'order', 'subject', 'created', 'copied_to_email')
+    def message_snippet(self, obj):
+        if not obj.message:
+            return ''
+        text = obj.message
+        return (text[:75] + '...') if len(text) > 75 else text
+
+    message_snippet.short_description = 'Message'
+
+    list_display = ('id', 'user', 'order', 'subject', 'message_snippet', 'created', 'copied_to_email')
     list_filter = ('copied_to_email', 'created')
     search_fields = ('user__username', 'user__email', 'order__order_number', 'subject', 'message')
+    readonly_fields = ('message', 'created',)
 
 
 @admin.register(ContactSettings)
 class ContactSettingsAdmin(admin.ModelAdmin):
     list_display = ('days_after_order', 'forward_to_email')
-from django.contrib import admin
