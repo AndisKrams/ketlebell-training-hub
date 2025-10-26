7b588fc5bcbf581258fdbc64425e9d62b55a41f5
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-26 13:58:23 +0000

    Added contact messages for admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ContactMessage, ContactSettings
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'order', 'subject', 'created', 'copied_to_email')
+    list_filter = ('copied_to_email', 'created')
+    search_fields = ('user__username', 'user__email', 'order__order_number', 'subject', 'message')
+
+
+@admin.register(ContactSettings)
+class ContactSettingsAdmin(admin.ModelAdmin):
+    list_display = ('days_after_order', 'forward_to_email')
+from django.contrib import admin
