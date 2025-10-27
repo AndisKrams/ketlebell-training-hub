a969ae03be7300debe186b3d54a7780e691858b4
AndisKrams <523ku12i@students.codeinstitute.net>
2025-10-27 16:50:13 +0000

    Create message text box for admin to read

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,14 +1,50 @@
 from django.contrib import admin
 
 from .models import Order, OrderLineItem
+from contact.models import ContactMessage
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('order_number', 'full_name', 'date', 'total', 'status', 'paid')
+    list_display = (
+        'order_number', 'full_name', 'date', 'total', 'status', 'paid'
+    )
     search_fields = ('order_number', 'full_name', 'email')
     list_filter = ('status', 'date', 'paid')
     actions = ['mark_as_dispatched']
+
+    # Show contact messages in the Order admin detail view so staff can
+    # read full customer messages alongside the order.
+    class ContactMessageInline(admin.StackedInline):
+        model = ContactMessage
+        fields = (
+            'user', 'subject', 'message_box', 'created', 'copied_to_email'
+        )
+        readonly_fields = (
+            'user',
+            'subject',
+            'message_box',
+            'created',
+            'copied_to_email',
+        )
+        extra = 0
+        can_delete = False
+
+        def message_box(self, obj):
+            from django.utils.html import escape
+            from django.utils.safestring import mark_safe
+
+            if not obj or not obj.message:
+                return ''
+            textarea = (
+                '<textarea readonly rows="6" cols="60" '
+                'style="width:100%">' + escape(obj.message) + '</textarea>'
+            )
+            return mark_safe(textarea)
+
+        message_box.short_description = 'Message'
+
+    inlines = [ContactMessageInline]
 
     def mark_as_dispatched(self, request, queryset):
         updated = queryset.update(status=Order.STATUS_DISPATCHED)
--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import ContactMessage, ContactSettings
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 
 @admin.register(ContactMessage)
@@ -12,10 +14,24 @@ class ContactMessageAdmin(admin.ModelAdmin):
 
     message_snippet.short_description = 'Message'
 
-    list_display = ('id', 'user', 'order', 'subject', 'message_snippet', 'created', 'copied_to_email')
+    def message_box(self, obj):
+        """Render the message inside a readonly textarea so it wraps nicely
+        and is fully visible in the admin detail view."""
+        if not obj or not obj.message:
+            return ''
+        # Escape to prevent XSS, but render inside a textarea for readability
+        return mark_safe(
+            f"<textarea readonly rows=8 cols=80 style=\"width:100%\">{escape(obj.message)}</textarea>"
+        )
+
+    message_box.short_description = 'Message'
+
+    list_display = (
+        'id', 'user', 'order', 'subject', 'message_snippet', 'created', 'copied_to_email'
+    )
     list_filter = ('copied_to_email', 'created')
     search_fields = ('user__username', 'user__email', 'order__order_number', 'subject', 'message')
-    readonly_fields = ('message', 'created',)
+    readonly_fields = ('message_box', 'created',)
 
 
 @admin.register(ContactSettings)
