a36de952d3515cecec55f6022a961b38cad0e087
AndisKrams <523ku12i@students.codeinstitute.net>
2025-12-16 14:07:53 +0000

    Removed email setting

--- kettlebell_hub/settings.py
+++ kettlebell_hub/settings.py
@@ -115,8 +115,6 @@ ACCOUNT_LOGIN_METHODS = {"username", "email"}
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_SIGNUP_FIELDS = ["email*", "email2*", "username*", "password1*", "password2*"]
 ACCOUNT_USERNAME_MIN_LENGTH = 4
-# Required for mandatory email verification
-ACCOUNT_EMAIL_REQUIRED = True
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 
